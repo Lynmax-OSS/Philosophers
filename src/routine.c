82514fca7447e8437df6faa9a0a1aed4aaf403a8
Keith <keith.teo2005@gmail.com>
2025-06-19 02:09:30 +0800

    made the routine for the philosophers

--- src/routine.c
+++ src/routine.c
@@ -12,14 +12,67 @@
 
 #include "../philosopher.h"
 
+void	philo_state(t_philo *philo, char *action)
+{
+	pthread_mutex_lock(&philo->data->print_mutex);
+	if (!philo->data->check_death)
+	{
+		printf("%ld %d %s\n",
+			get_time_in_ms() - philo->data->start_time,
+			philo->id, action);
+	}
+	pthread_mutex_unlock(&philo->data->print_mutex);
+}
+
+void	pick_even_fork(t_philo *philo)
+{
+	pthread_mutex_lock(&philo->r_fork->mutex);
+	philo_state(philo, "has taken a fork");
+	pthread_mutex_lock(&philo->l_fork->mutex);
+	philo_state(philo, "has taken a fork");
+}
+
+void	pick_odd_fork(t_philo *philo)
+{
+	pthread_mutex_lock(&philo->r_fork->mutex);
+	philo_state(philo, "has taken a fork");
+	pthread_mutex_lock(&philo->l_fork->mutex);
+	philo_state(philo, "has taken a fork");
+}
+
+void	put_down_forks(t_philo *philo)
+{
+	pthread_mutex_unlock(&philo->l_fork->mutex);
+	pthread_mutex_unlock(&philo->r_fork->mutex);
+}
+
+void	pick_up_forks(t_philo *philo)
+{
+	int	even;
+
+	even = (philo->id % 2 == 0);
+	if (even)
+		pick_even_fork(philo);
+	else
+		pick_odd_fork(philo);
+}
+
 void	routine(void *arg)
 {
-	t_philo	philo;
+	t_philo	*philo;
 
-	pthread_mutex_lock(philo.l_fork);
-	pthread_mutex_lock(philo.r_fork);
-	philo_state(philo.id + 1, get_time_in_ms() - philo.data->start_time, "is eating");
-	pthread_mutex_unlock(philo.l_fork);
-	pthread_mutex_unlock(philo.r_fork);
-	philo_state(philo.id + 1, get_time_in_ms() - philo.data->start_time, "is sleeping");
+	philo = (t_philo *)arg;
+	philo->last_meal = get_time_in_ms();
+	while (!philo->data->check_death)
+	{
+		pick_up_forks(philo);
+		philo_state(philo, "is eating");
+		philo->last_meal = get_time_in_ms();
+		precise_usleep(philo->data->tte);
+		put_down_forks(philo);
+		philo_state(philo, "is sleeping");
+		precise_usleep(philo->data->tts);
+		philo_state(philo, "is thinking");
+	}
+	return (NULL);
 }
