f39e1147667c52b127c36b56d041fee287d9b6fb
Keith Teo <keteo@u81z04s01.42kl.edu.my>
2025-06-18 16:38:25 +0800

    initial stage of routine and funtion to write the state of philosopher

--- src/routine.c
+++ src/routine.c
@@ -12,9 +12,14 @@
 
 #include "../philosopher.h"
 
-void	routine(void arg)
+void	routine(void *arg)
 {
-	t_data data;
+	t_philo	philo;
 
-	
+	pthread_mutex_lock(philo.l_fork);
+	pthread_mutex_lock(philo.r_fork);
+	philo_state(philo.id + 1, get_time_in_ms() - philo.data->start_time, "is eating");
+	pthread_mutex_unlock(philo.l_fork);
+	pthread_mutex_unlock(philo.r_fork);
+	philo_state(philo.id + 1, get_time_in_ms() - philo.data->start_time, "is sleeping");
 }
