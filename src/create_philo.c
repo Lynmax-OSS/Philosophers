9230f36450d82b3432ee23fcc00cbfc8748f0986
Keith <keith.teo2005@gmail.com>
2025-06-25 18:18:06 +0800

    split the thread creation to another function

---
+++ src/create_philo.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   create_philo.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: keteo <keteo@student.42kl.edu.my>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/25 17:06:38 by keteo             #+#    #+#             */
+/*   Updated: 2025/06/25 17:06:38 by keteo            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../philosopher.h"
+
+void	create_philo(t_data *data)
+{
+	int	i;
+
+	if (data->nop == 1)
+		pthread_create(&data->philo[i].thread,
+					NULL, single_philo_routine, &data->philo[i]);
+	else	
+	{
+		while (i < data->nop)
+		{
+			pthread_create(&data->philo[i].thread,
+						NULL, routine, &data->philo[i]);
+			i++;
+		}
+	}
+}
