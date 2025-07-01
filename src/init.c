7719f711a2d733160b162afdf9a342c94cc42750
Keith <keith.teo2005@gmail.com>
2025-07-01 11:46:16 +0800

    revamped init.c

--- src/init.c
+++ src/init.c
@@ -12,29 +12,6 @@
 
 #include "../philosopher.h"
 
-int	validate_args(char **av)
-{
-	int	i;
-	int	j;
-
-	i = 1;
-	while (av[i])
-	{
-		j = 0;
-		while (av[i][j])
-		{
-			if (av[i][j] < '0' || av[i][j] > '9')
-			{
-				write(2, "Error: invalid argument\n", 25);
-				return (1);
-			}
-			j++;
-		}
-		i++;
-	}
-	return (0);
-}
-
 static int	int_atoi(char *s)
 {
 	int		i;
@@ -53,54 +30,112 @@ static int	int_atoi(char *s)
 	return ((int)result);
 }
 
-static void	set_philo(t_data *data, int i)
+static int	validate_args(int argc, char **argv)
 {
-	pthread_mutex_init(&data->fork[i].mutex, NULL);
-	pthread_mutex_init(&data->philo[i].meal_mutex, NULL);
-	data->philo[i].id = i + 1;
-	data->philo[i].l_fork = &data->fork[i];
-	data->philo[i].r_fork = &data->fork[(i + 1) % data->nop];
-	data->philo[i].data = data;
-}
+	int i;
 
-static int	setup(t_data *data)
-{
-	int	i;
-
-	i = 0;
-	data->fork = malloc(sizeof(t_fork) * data->nop);
-	data->philo = malloc(sizeof(t_philo) * data->nop);
-	if (!data->fork || !data->philo)
-		return (0);
-	pthread_mutex_init(&data->print_mutex, NULL);
-	while (i < data->nop)
+	i = 1;
+	while (i < argc)
 	{
-		set_philo(data, i);
+		int j = 0;
+		while (argv[i][j])
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				write(2, "Error: arguments not integers\n", 31);
+				return (1);
+			}
+			j++;
+		}
 		i++;
 	}
-	return (1);
+	return (0);
 }
 
-int	init(t_data *data, int ac, char **av)
+int	parse_args(int argc, char **argv, t_data *data)
 {
-	if (ac != 5)
+	if (argc < 5 || argc > 6)
 	{
-		write(2, "./philo: <philo> <die> <eat> <sleep>\n", 38);
-		return (0);
-	}
-	if (validate_args(av))
+		write(2, "Error: Not enough arguments\n", 29);
 		return (1);
-	data->nop = int_atoi(av[1]);
-	data->ttd = int_atoi(av[2]);
-	data->tte = int_atoi(av[3]);
-	data->tts = int_atoi(av[4]);
-	data->check_death = 0;
-	if (data->nop <= 0 || data->ttd <= 0 || data->tte <= 0 || data->tts <= 0)
+	}
+	if (validate_args(argc, argv))
+		return (1);
+	data->nop = int_atoi(argv[1]);
+	data->ttd = int_atoi(argv[2]);
+	data->tte = int_atoi(argv[3]);
+	data->tts = int_atoi(argv[4]);
+	data->meal_limit = -1;
+	if (argc == 6)
+		data->meal_limit = int_atoi(argv[5]);
+	if (data->nop <= 0 || data->ttd <= 0 ||
+		data->tte <= 0 || data->tts <= 0 ||
+		(argc == 6 && data->meal_limit <= 0))
 	{
 		write(2, "Error: arguments must be positive integers\n", 44);
 		return (1);
 	}
-	if (!setup(data))
+	return (0);
+}
+
+int	init(t_data *data)
+{
+	int i;
+	i = 0;
+	data->check_death = 0;
+	data->reach_limit = 0;
+	pthread_mutex_init(&data->print_mutex, NULL);
+	data->fork = malloc(sizeof(t_fork) * data->nop);
+	data->philo = malloc(sizeof(t_philo) * data->nop);
+	if (!data->fork || !data->philo)
 		return (1);
+	while (i < data->nop)
+	{
+		pthread_mutex_init(&data->fork[i].mutex, NULL);
+		i++;
+	}
+	i = 0;
+	while (i < data->nop)
+	{
+		data->philo[i].id = i + 1;
+		data->philo[i].meals_eaten = 0;
+		data->philo[i].l_fork = &data->fork[i];
+		data->philo[i].r_fork = &data->fork[(i + 1) % data->nop];
+		data->philo[i].data = data;
+		data->philo[i].full = 0;
+		pthread_mutex_init(&data->philo[i].meal_mutex, NULL);
+		i++;
+	}
+	return (0);
+}
+
+int	start_simulation(t_data *data)
+{
+	int								i;
+	pthread_t					monitor_thread;
+
+	data->start_time = get_time_in_ms();
+	i = 0;
+	if (data->nop == 1)
+	{
+		pthread_create(&data->philo[0].thread, NULL,
+			single_philo_routine, &data->philo[0]);
+		pthread_join(data->philo[0].thread, NULL);
+		return (0);
+	}
+	while (i < data->nop)
+	{
+		pthread_create(&data->philo[i].thread, NULL,
+			routine, &data->philo[i]);
+		i++;
+	}
+	pthread_create(&monitor_thread, NULL, monitor, data);
+	pthread_join(monitor_thread, NULL);
+	i = 0;
+	while (i < data->nop)
+	{
+		pthread_join(data->philo[i].thread, NULL);
+		i++;
+	}
 	return (0);
 }
