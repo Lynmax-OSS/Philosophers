8cdc7be50c1c07e2c115ce144bd07f9483f69067
Keith <keith.teo2005@gmail.com>
2025-07-01 11:45:34 +0800

    trying to implement the meal limit

--- src/monitor.c
+++ src/monitor.c
@@ -12,13 +12,20 @@
 
 #include "../philosopher.h"
 
-void	moniter_loop(t_data *data, int i)
+void	moniter_loop(t_data *data, int i, int *full_count)
 {
+	long	last_meal;
 	long	time_since_meal;
 
 	pthread_mutex_lock(&data->philo[i].meal_mutex);
-	time_since_meal = get_time_in_ms() - data->philo[i].last_meal;
+	last_meal = data->philo[i].last_meal;
+	if (data->philo[i].full)
+	{
+		printf("philo %d is full\n", data->philo->id);
+		(*full_count)++;
+	}
 	pthread_mutex_unlock(&data->philo[i].meal_mutex);
+	time_since_meal = get_time_in_ms() - last_meal;
 	if (time_since_meal > data->ttd)
 	{
 		pthread_mutex_lock(&data->print_mutex);
@@ -30,20 +37,30 @@ void	moniter_loop(t_data *data, int i)
 	}
 }
 
-void	*moniter(void *arg)
+void	*monitor(void *arg)
 {
 	t_data	*data;
 	int		i;
+	int		full_count;
 
 	data = (t_data *)arg;
 	i = 0;
 	while (!data->check_death)
 	{
-		moniter_loop(data, i);
+		full_count = 0;
+		moniter_loop(data, i, &full_count);
 		i++;
 		if (i >= data->nop)
+		{
 			i = 0;
-		usleep(1000);
+			if (data->meal_limit > 0 && full_count >= data->nop)
+			{
+				printf("All philosophers ate enough. Ending simulation.\n");
+				data->reach_limit = 1;
+				break;
+			}
+		}
+		precise_usleep(1000);
 	}
 	return (NULL);
 }
