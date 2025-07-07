69959c68075a1fb7406ef21b286f175e968de4e6
Keith <keith.teo2005@gmail.com>
2025-07-07 13:29:51 +0800

    shortened routine by cutting down unneccessary if statements and splitting eating into seprate helper function

--- src/routine.c
+++ src/routine.c
@@ -24,7 +24,7 @@ void	*single_philo_routine(void *arg)
 	return (NULL);
 }
 
-void	set_full(t_philo *philo)
+int	set_full(t_philo *philo)
 {
 	if (philo->data->meal_limit > 0 && philo->meals_eaten
 		== philo->data->meal_limit && philo->full != 1)
@@ -32,7 +32,19 @@ void	set_full(t_philo *philo)
 		pthread_mutex_lock(&philo->full_mutex);
 		philo->full = 1;
 		pthread_mutex_unlock(&philo->full_mutex);
+		return (0);
 	}
+	return (1);
+}
+
+void	philo_eating(t_philo *philo)
+{
+	philo_state(philo, "is eating");
+	pthread_mutex_lock(&philo->meal_mutex);
+	philo->last_meal = get_time_in_ms();
+	philo->meals_eaten++;
+	pthread_mutex_unlock(&philo->meal_mutex);
+	precise_usleep(philo->data->tte);
 }
 
 void	*routine(void *arg)
@@ -42,33 +54,19 @@ void	*routine(void *arg)
 	philo = (t_philo *)arg;
 	while (!philo->data->check_death)
 	{
-		if (philo->data->meal_limit > 0 && philo->meals_eaten >= philo->data->meal_limit)
-		{
-			set_full(philo);
+		if (!set_full(philo))
 			return (NULL);
-		}
 		if (philo->id % 2 == 0)
 			precise_usleep(100);
 		pick_up_forks(philo);
-		philo_state(philo, "is eating");
-		pthread_mutex_lock(&philo->meal_mutex);
-		philo->last_meal = get_time_in_ms();
-		philo->meals_eaten++;
-		pthread_mutex_unlock(&philo->meal_mutex);
-		precise_usleep(philo->data->tte);
+		philo_eating(philo);
 		put_down_forks(philo);
-		if (philo->data->meal_limit > 0 && philo->meals_eaten >= philo->data->meal_limit)
-		{
-			set_full(philo);
+		if (!set_full(philo))
 			return (NULL);
-		}
 		philo_state(philo, "is sleeping");
 		precise_usleep(philo->data->tts);
-		if (philo->data->meal_limit > 0 && philo->meals_eaten >= philo->data->meal_limit)
-		{
-			set_full(philo);
+		if (!set_full(philo))
 			return (NULL);
-		}
 		philo_state(philo, "is thinking");
 		if (philo->data->nop % 2 != 0 && philo->id % 2 == 1)
 			precise_usleep(philo->data->tte / 2);
